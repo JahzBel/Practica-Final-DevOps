a6bc21df0c062ba3259a4dca8ce8444c2eabb3c4
JahzBel <128754281+JahzBel@users.noreply.github.com>
2025-08-21 19:50:22 -0400

    Deploying To Render

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,19 @@
 const request = require("supertest");
 const app = require("../index");
 
+let server;
+
+beforeAll(() => {
+  server = app.listen(3001); // puerto de prueba
+});
+
+afterAll((done) => {
+  server.close(done);
+});
+
 describe("GET /", () => {
   it("Debe responder con Hola Mundo", async () => {
-    const res = await request(app).get("/");
+    const res = await request(server).get("/");
     expect(res.statusCode).toBe(200);
     expect(res.text).toContain("Hola Mundo");
   });
